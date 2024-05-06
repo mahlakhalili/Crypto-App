dba8a5ffc5ae9e88fd671c9af3e09ff8cf82d5eb
Mahla <115685227+mahlakhalili@users.noreply.github.com>
2024-05-06 04:05:02 -0700

    Update HomePage.jsx

--- src/Templates/HomePage.jsx
+++ src/Templates/HomePage.jsx
@@ -1,15 +1,17 @@
 import  { useEffect, useState } from 'react'
 import TableCoin from '../Modules/TableCoin'
+import { getCoinList } from '../services/cryptoApi'
 
 const HomePage = () => {
     const [coins,setCoins] = useState([])
 
     useEffect(()=>{
-        fetch(
-            "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=20&page=20&x_cg_demo_api_key=CG-6RAubenEiFsz29R5G3Khy4pP"
-        )
-        .then((res)=> res.json())
-        .then((json)=> setCoins(json))
+      const getData = async ()=>{
+        const res = await fetch(getCoinList())
+        const json = await res.json()
+        setCoins(json)
+      }
+        getData()
     } , [])
   return (
     <div>
