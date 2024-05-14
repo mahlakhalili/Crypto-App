db2443141a5a898c3dbba6ef02586de411291800
Mahla <115685227+mahlakhalili@users.noreply.github.com>
2024-05-14 01:00:40 -0700

    Update HomePage.jsx

--- src/Templates/HomePage.jsx
+++ src/Templates/HomePage.jsx
@@ -1,25 +1,28 @@
-import  { useEffect, useState } from 'react'
-import TableCoin from '../Modules/TableCoin'
-import { getCoinList } from '../services/cryptoApi'
+import { useEffect, useState } from "react";
+import TableCoin from "../Modules/TableCoin";
+import { getCoinList } from "../services/cryptoApi";
+import Pagination from "../Modules/Pagination";
 
 const HomePage = () => {
-    const [coins,setCoins] = useState([])
+  const [coins, setCoins] = useState([]);
+  const [isLoading , setIsLoading] = useState(true)
 
-    useEffect(()=>{
-      const getData = async ()=>{
-        const res = await fetch(getCoinList())
-        const json = await res.json()
-        setCoins(json)
-        console.log(json)
-      }
-        getData()
-        
-    } , [])
+  useEffect(() => {
+    const getData = async () => {
+      const res = await fetch(getCoinList());
+      const json = await res.json();
+      setCoins(json);
+      setIsLoading(false)
+      console.log(json);
+    };
+    getData();
+  }, []);
   return (
     <div>
-        <TableCoin coins = {coins}/>
+      <Pagination />
+      <TableCoin coins={coins} isLoading = {isLoading} />
     </div>
-  )
-}
+  );
+};
 
-export default HomePage
+export default HomePage;
