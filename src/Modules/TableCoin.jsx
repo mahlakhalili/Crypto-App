02354057609c301a9a2fdd41dc102bbef8222a7c
Mahla <115685227+mahlakhalili@users.noreply.github.com>
2024-05-18 00:22:50 -0700

    Update TableCoin.jsx

--- src/Modules/TableCoin.jsx
+++ src/Modules/TableCoin.jsx
@@ -1,58 +1,67 @@
 import chartUp from "../assets/chart-up.svg";
 import chartDown from "../assets/chart-down.svg";
-import {BallTriangle} from 'react-loader-spinner';
+import { BallTriangle } from "react-loader-spinner";
+import PropTypes from "prop-types";
 
-const TableCoin = ({ coins , isLoading }) => {
+import styles from "./TableCoin.module.css";
+
+const TableCoin = ({ coins, isLoading }) => {
   return (
-    <div>
-		{
-			isLoading ? <BallTriangle /> : (
-				<table>
-        <thead>
-          <tr>
-            <th>Coin</th>
-            <th>Name</th>
-            <th>Price</th>
-            <th>24h</th>
-            <th>Total Volume</th>
-            <th></th>
-          </tr>
-        </thead>
-        <tbody>
-          {coins.map((coin) => (
-            <TableRow coin={coin} key={coin.id} />
-          ))}
-        </tbody>
-      </table>
-			)
-		}
+    <div className={styles.container}>
+      {isLoading ? (
+        <BallTriangle />
+      ) : (
+        <table className={styles.table}>
+          <thead>
+            <tr>
+              <th>Coin</th>
+              <th>Name</th>
+              <th>Price</th>
+              <th>24h</th>
+              <th>Total Volume</th>
+              <th></th>
+            </tr>
+          </thead>
+          <tbody>
+            {coins.map((coin) => (
+              <TableRow coin={coin} key={coin.id} />
+            ))}
+          </tbody>
+        </table>
+      )}
     </div>
   );
+};
+
+TableCoin.PropTypes = {
+  coin: PropTypes.object,
 };
 
 export default TableCoin;
 
 const TableRow = ({
-	coin: {
+  coin: {
     name,
     image,
     symbol,
     total_volume,
     current_price,
     price_change_percentage_24h: price_change,
-	},
+  },
 }) => {
-	return (
+  return (
     <tr>
-    	<td>
-        <div>
-        	<img src={image} alt="" />
-        	<span>{symbol.toUpperCase()}</span>
+      <td>
+        <div className={styles.symbol}>
+          <img src={image} alt="" />
+          <span>{symbol.toUpperCase()}</span>
         </div>
       </td>
       <td>{name}</td>
       <td>${current_price.toLocaleString()}</td>
-      <td>{price_change.toFixed(2)}%</td>
+      <td className={price_change > 0 ? styles.success : styles.error}>
+        {price_change.toFixed(2)}%
+      </td>
       <td>{total_volume.toLocaleString()}</td>
       <td>
         <img src={price_change > 0 ? chartUp : chartDown} alt="" />
