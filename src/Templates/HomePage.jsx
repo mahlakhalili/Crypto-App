530b497548b54edbf1632b5a1ee177128adce400
Mahla <115685227+mahlakhalili@users.noreply.github.com>
2024-05-19 02:26:13 -0700

    Update HomePage.jsx

--- src/Templates/HomePage.jsx
+++ src/Templates/HomePage.jsx
@@ -6,20 +6,22 @@ import Pagination from "../Modules/Pagination";
 const HomePage = () => {
   const [coins, setCoins] = useState([]);
   const [isLoading , setIsLoading] = useState(true)
+  const [page , setPage] = useState(1)
 
   useEffect(() => {
+    setIsLoading(true)
     const getData = async () => {
-      const res = await fetch(getCoinList());
+      const res = await fetch(getCoinList(page));
       const json = await res.json();
       setCoins(json);
       setIsLoading(false)
       console.log(json);
     };
     getData();
-  }, []);
+  }, [page]);
   return (
     <div>
-      <Pagination />
+      <Pagination page = {page} setPage = {setPage}/>
       <TableCoin coins={coins} isLoading = {isLoading} />
     </div>
   );
