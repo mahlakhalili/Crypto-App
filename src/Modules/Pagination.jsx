c19f0c1b10e4becf31437d4f9969a479e8eb4a83
Mahla <115685227+mahlakhalili@users.noreply.github.com>
2024-05-20 05:39:09 -0700

    Update Pagination.jsx

--- src/Modules/Pagination.jsx
+++ src/Modules/Pagination.jsx
@@ -1,35 +1,41 @@
 import React from "react";
+import styles from "./Pagination.module.css";
 
-
-const Pagination = ({page , setPage})=>  {
+const Pagination = ({ page, setPage }) => {
   const preHandler = () => {
-    if(page <= 1 ) return
-    setPage((page) => page - 1)
+    if (page <= 1) return;
+    setPage((page) => page - 1);
   };
   const nextHandler = () => {
-    if(page >= 10) return
-    setPage((page) => page + 1)
+    if (page >= 10) return;
+    setPage((page) => page + 1);
   };
 
-
   return (
-    <div>
-      <button onClick={preHandler}>Previous</button>
-      <p style={{color : page == 1 ? "red" : "inherit"}}>1</p>
-      <p style={{color : page == 2 ? "red" : "inherit"}}>2</p>
-      {
-        page > 2 && page < 9 &&
-        (
-            <>
-                <span>....</span>
-                <p>{page}</p>
-            </>
-        )
-      }
+    <div className={styles.pagination}>
+      <button
+        onClick={preHandler}
+        className={page == 1 ? styles.disabled : null}
+      >
+        Previous
+      </button>
+      <p className={page == 1 ? styles.selected : null}>1</p>
+      <p className={page == 2 ? styles.selected : null}>2</p>
+      {page > 2 && page < 9 && (
+        <>
+          <span>....</span>
+          <p className={styles.selected}>{page}</p>
+        </>
+      )}
       <span>....</span>
-      <p>9</p>
-      <p>10</p>
-      <button onClick={nextHandler}>Next</button>
+      <p className={page == 9 ? styles.selected : null}>9</p>
+      <p className={page == 10 ? styles.selected : null}>10</p>
+      <button
+        onClick={nextHandler}
+        className={page == 10 ? styles.disabled : null}
+      >
+        Next
+      </button>
     </div>
   );
 };
