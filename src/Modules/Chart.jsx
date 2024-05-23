a7c86ac630b99a973f0ce35d24bf6c9885c712c8
Mahla <115685227+mahlakhalili@users.noreply.github.com>
2024-05-23 03:36:23 -0700

    Create Chart.jsx

---
+++ src/Modules/Chart.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import styles from "./Chart.module.css";
+
+const Chart = ({ chart, setChart }) => {
+  return (
+    <div className={styles.container}>
+      <span className={styles.cross} onClick={() => setChart(null)}>X</span>
+      <div className={styles.chart}></div>
+    </div>
+  );
+};
+
+export default Chart;
