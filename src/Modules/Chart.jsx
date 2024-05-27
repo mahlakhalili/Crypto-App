2fe7e810e31f7a42c2e8246a3f18dcc3d2292e08
Mahla <115685227+mahlakhalili@users.noreply.github.com>
2024-05-27 00:38:26 -0700

    Update Chart.jsx

--- src/Modules/Chart.jsx
+++ src/Modules/Chart.jsx
@@ -1,10 +1,16 @@
 import React from "react";
 import styles from "./Chart.module.css";
+import { convertData } from "../helpers/convertData";
+import { useState } from "react";
 
 const Chart = ({ chart, setChart }) => {
+  const [type, setType] = useState("prices");
+  console.log(convertData(chart, type));
   return (
     <div className={styles.container}>
-      <span className={styles.cross} onClick={() => setChart(null)}>X</span>
+      <span className={styles.cross} onClick={() => setChart(null)}>
+        X
+      </span>
       <div className={styles.chart}></div>
     </div>
   );
