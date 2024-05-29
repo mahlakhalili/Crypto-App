865261b62e89f8b25ee2910875bef1da6354d10a
Mahla <115685227+mahlakhalili@users.noreply.github.com>
2024-05-29 05:50:05 -0700

    Update cryptoApi.js

--- src/services/cryptoApi.js
+++ src/services/cryptoApi.js
@@ -1,7 +1,7 @@
 const BASE_URL = "https://api.coingecko.com/api/v3";
 const API_KEY = "CG-RFaD9bpZzFAbcXXKtoet3M9e";
 const getCoinList = (page , currency) => {
-  return `${BASE_URL}/coins/markets?vs_currency=${currency}&x_page=20&page=${page}_cg_demo_api_key=${API_KEY}`;
+  return `${BASE_URL}/coins/markets?vs_currency=${currency}&per_page=20&page=${page}_cg_demo_api_key=${API_KEY}`;
 };
 const searchCoin = query => `${BASE_URL}/search?query=${query}&x_cg_demo_api_key=${API_KEY}`;
 const marketChart = coin => `${BASE_URL}/coins/${coin}/market_chart?vs_currency=usd&days=7`
