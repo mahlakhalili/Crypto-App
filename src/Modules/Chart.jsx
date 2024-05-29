92947b426803d51fb3878972dd1ffd4f0a34d7c8
Mahla <115685227+mahlakhalili@users.noreply.github.com>
2024-05-29 06:04:02 -0700

    Update Chart.jsx

--- src/Modules/Chart.jsx
+++ src/Modules/Chart.jsx
@@ -33,15 +33,21 @@ const Chart = ({ chart, setChart }) => {
 export default Chart;
 
 const ChartComponent = ({ data, type }) => {
-  return
-  <ResponsiveContainer width="100%" height="100%">
-    <LineChart width={400} height={400} data={data}>
-      <Line type="monotone" dataKey={type} stroke="#3874ff" strokeWidth="2px" />
-      <CartesianGrid stroke="#404042" />
-      <YAxis dataKey={type} domain={["auto", "auto"]} />
-      <XAxis dataKey="date" />
-      <Legend />
-      <Tooltip />
-    </LineChart>
-  </ResponsiveContainer>;
+  return (
+    <ResponsiveContainer width="100%" height="100%">
+      <LineChart width={400} height={400} data={data}>
+        <Line
+          type="monotone"
+          dataKey={type}
+          stroke="#3874ff"
+          strokeWidth="2px"
+        />
+        <CartesianGrid stroke="#404042" />
+        <YAxis dataKey={type} domain={["auto", "auto"]} />
+        <XAxis dataKey="date" />
+        <Legend />
+        <Tooltip />
+      </LineChart>
+    </ResponsiveContainer>
+  );
 };
