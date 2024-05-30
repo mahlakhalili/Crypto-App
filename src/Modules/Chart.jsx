ca676b5c35d7a32bb7de9268683bba302967afe7
Mahla <115685227+mahlakhalili@users.noreply.github.com>
2024-05-30 05:06:26 -0700

    Update Chart.jsx

--- src/Modules/Chart.jsx
+++ src/Modules/Chart.jsx
@@ -21,9 +21,32 @@ const Chart = ({ chart, setChart }) => {
       <span className={styles.cross} onClick={() => setChart(null)}>
         X
       </span>
+      <div className={styles.name}>
+        <img src={chart.coin.image} />
+        <p>{chart.coin.name}</p>
+      </div>
       <div className={styles.chart}>
         <div className={styles.graph}>
           <ChartComponent data={convertData(chart, type)} type={type} />
+        </div>
+        <div className={styles.types}>
+          <button>Prices</button>
+          <button>Market Caps</button>
+          <button>Total Volumes</button>
+        </div>
+        <div className={styles.details}>
+          <div>
+            <p>Prices : </p>
+            <span>${chart.coin.current_price}</span>
+          </div>
+          <div>
+            <p>ATH : </p>
+            <span>${chart.coin.ath}</span>
+          </div>
+          <div>
+            <p>Market Cap : </p>
+            <span>${chart.coin.market_cap}</span>
+          </div>
         </div>
       </div>
     </div>
